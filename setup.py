791e32a23b39dad417fb2793e0956aa86c7a816a
SuHun Han <ssut@ssut.me>
2017-03-10 23:15:21 +0900

    Minor updates for package
    - Change the package name to googletrans
    - reStructedText README for PyPI
    - Travis CI

--- setup.py
+++ setup.py
@@ -1,38 +1,56 @@
 #!/usr/bin/env python
 import googletrans
+import os.path
 from setuptools import setup, find_packages
 
+
+def readme():
+    path = os.path.join(os.path.dirname(__file__), 'README.rst')
+    try:
+        with open(path) as f:
+            return f.read()
+    except IOError:
+        pass
+
+
 def install():
-    desc = googletrans.__doc__
     setup(
-        name='py-googletrans',
-        version='1.2',
-        description=desc,
-        long_description=desc,
+        name='googletrans',
+        version=googletrans.__version__,
+        description=googletrans.__doc__,
+        long_description=readme(),
+        license='MIT',
         author='SuHun Han',
-        author_email='ssut@ssut.me',
+        author_email='ssut' '@' 'ssut.me',
         url='https://github.com/ssut/py-googletrans',
-        classifiers = ['Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Education',
-            'Intended Audience :: End Users/Desktop',
-            'License :: Freeware',
-            'Operating System :: POSIX',
-            'Operating System :: Microsoft :: Windows',
-            'Operating System :: MacOS :: MacOS X',
-            'Topic :: Education',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.2',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4'],
-        packages=find_packages(),
+        classifiers=['Development Status :: 5 - Production/Stable',
+                     'Intended Audience :: Education',
+                     'Intended Audience :: End Users/Desktop',
+                     'License :: Freeware',
+                     'Operating System :: POSIX',
+                     'Operating System :: Microsoft :: Windows',
+                     'Operating System :: MacOS :: MacOS X',
+                     'Topic :: Education',
+                     'Programming Language :: Python',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3.4',
+                     'Programming Language :: Python :: 3.5',
+                     'Programming Language :: Python :: 3.6'],
+        packages=find_packages(exclude=['docs', 'tests']),
+        keywords='google translate translator',
         install_requires=[
             'requests',
-            'future',
         ],
-        scripts=['translate'],
+        extras_require={
+            'h2': ['hyper'],
+        },
+        tests_require=[
+            'pytest',
+            'coveralls',
+        ],
+        scripts=['translate']
     )
+
 
 if __name__ == "__main__":
     install()

ssut/py-googletrans

commit 04a0e6dfbbeb7993681a8e2dd656aa40bcb0717b

Support languages names and RFC 1766 codes (#26)
* Update client.py

Accept languages names as dest/src params in the `translate` routine.
Move dest/src params check to the `translate` routine.

* Update constants.py

Add LANGCODES dict

* Update __init__.py

Import LANGCODES

* Update test_client.py

Add tests

* Update constants.py

Fix typo

googletrans/constants.py
@@ -60,3 +60,5 @@ LANGUAGES = {
     'cy': 'welsh',
     'yi': 'yiddish',
   }
+
+LANGCODES = dict(map(reversed, LANGUAGES.items()))

tests/test_client.py
@@ -27,6 +27,16 @@ def test_unicode(translator):
     assert result.text == u'こんにちは。'
 
 
+def test_language_name(translator):
+    result = translator.translate(u'Hello', src='ENGLISH', dest='iRiSh')
+    assert result.text == u'Dia dhuit'
+
+
+def test_language_rfc1766(translator):
+    result = translator.translate(u'luna', src='it_ch@euro', dest='en')
+    assert result.text == u'moon'
+
+
 def test_special_chars(translator):
     text = u"©×《》"
 

googletrans/__init__.py
@@ -4,4 +4,4 @@ __version__ = '2.1.3'
 
 
 from googletrans.client import Translator
-from googletrans.constants import LANGUAGES
+from googletrans.constants import LANGCODES, LANGUAGES

googletrans/client.py
@@ -10,7 +10,7 @@ import random
 from googletrans import urls, utils
 from googletrans.compat import PY3
 from googletrans.gtoken import TokenAcquirer
-from googletrans.constants import DEFAULT_USER_AGENT, LANGUAGES, SPECIAL_CASES
+from googletrans.constants import DEFAULT_USER_AGENT, LANGCODES, LANGUAGES, SPECIAL_CASES
 from googletrans.models import Translated, Detected
 
 
@@ -50,19 +50,7 @@ class Translator(object):
             return self.service_urls[0]
         return random.choice(self.service_urls)
 
-    def _translate(self, text, dest='en', src='auto'):
-        if src != 'auto':
-            if src not in LANGUAGES.keys() and src in SPECIAL_CASES.keys():
-                src = SPECIAL_CASES[src]
-            elif src not in LANGUAGES.keys():
-                raise ValueError('invalid source language')
-
-        if dest not in LANGUAGES.keys():
-            if dest in SPECIAL_CASES.keys():
-                dest = SPECIAL_CASES[dest]
-            else:
-                raise ValueError('invalid destination language')
-
+    def _translate(self, text, dest, src):
         if not PY3 and isinstance(text, str):  # pragma: nocover
             text = text.decode('utf-8')
 
@@ -82,11 +70,13 @@ class Translator(object):
         :type text: UTF-8 :class:`str`; :class:`unicode`; string sequence (list, tuple, iterator, generator)
 
         :param dest: The language to translate the source text into.
-                     The value should be one of the language codes listed in :const:`googletrans.LANGUAGES`.
+                     The value should be one of the language codes listed in :const:`googletrans.LANGUAGES`
+                     or one of the language names listed in :const:`googletrans.LANGCODES`.
         :param dest: :class:`str`; :class:`unicode`
 
         :param src: The language of the source text.
-                    The value should be one of the language codes listed in :const:`googletrans.LANGUAGES`.
+                    The value should be one of the language codes listed in :const:`googletrans.LANGUAGES`
+                    or one of the language names listed in :const:`googletrans.LANGCODES`.
                     If a language is not specified,
                     the system will attempt to identify the source language automatically.
         :param src: :class:`str`; :class:`unicode`
@@ -112,6 +102,25 @@ class Translator(object):
             jumps over  ->  이상 점프
             the lazy dog  ->  게으른 개
         """
+        dest = dest.lower().split('_', 1)[0]
+        src = src.lower().split('_', 1)[0]
+
+        if src != 'auto' and src not in LANGUAGES:
+            if src in SPECIAL_CASES:
+                src = SPECIAL_CASES[src]
+            elif src in LANGCODES:
+                src = LANGCODES[src]
+            else:
+                raise ValueError('invalid source language')
+
+        if dest not in LANGUAGES:
+            if dest in SPECIAL_CASES:
+                dest = SPECIAL_CASES[dest]
+            elif dest in LANGCODES:
+                dest = LANGCODES[dest]
+            else:
+                raise ValueError('invalid destination language')
+
         if isinstance(text, list):
             result = []
             for item in text:

commit 066eff62931fc0f116193dec7314610cec60b849

Update language names to match Google Translate (#33)
* Change language names to match Google Translate

This commit also fixes an issue introduced by commit 04a0e6df (issue #26),
where language names with underscores in them would not work, because
everything after an underscore is ignored.

* Add test for language names with spaces in them

googletrans/constants.py
@@ -19,8 +19,8 @@ LANGUAGES = {
     'ca': 'catalan',
     'ceb': 'cebuano',
     'ny': 'chichewa',
-    'zh-CN': 'chinese_simplified',
-    'zh-TW': 'chinese_traditional',
+    'zh-cn': 'chinese (simplified)',
+    'zh-tw': 'chinese (traditional)',
     'co': 'corsican',
     'hr': 'croatian',
     'cs': 'czech',
@@ -38,7 +38,7 @@ LANGUAGES = {
     'de': 'german',
     'el': 'greek',
     'gu': 'gujarati',
-    'ht': 'haitian_creole',
+    'ht': 'haitian creole',
     'ha': 'hausa',
     'haw': 'hawaiian',
     'iw': 'hebrew',
@@ -56,7 +56,7 @@ LANGUAGES = {
     'kk': 'kazakh',
     'km': 'khmer',
     'ko': 'korean',
-    'ku': 'kurdish',
+    'ku': 'kurdish (kurmanji)',
     'ky': 'kyrgyz',
     'lo': 'lao',
     'la': 'latin',
@@ -71,7 +71,7 @@ LANGUAGES = {
     'mi': 'maori',
     'mr': 'marathi',
     'mn': 'mongolian',
-    'my': 'myanmar',
+    'my': 'myanmar (burmese)',
     'ne': 'nepali',
     'no': 'norwegian',
     'ps': 'pashto',
@@ -82,7 +82,7 @@ LANGUAGES = {
     'ro': 'romanian',
     'ru': 'russian',
     'sm': 'samoan',
-    'gd': 'scots_gaelic',
+    'gd': 'scots gaelic',
     'sr': 'serbian',
     'st': 'sesotho',
     'sn': 'shona',

tests/test_client.py
@@ -42,6 +42,11 @@ def test_language_name(translator):
     assert result.text == u'Dia dhuit'
 
 
+def test_language_name_with_space(translator):
+    result = translator.translate(u'Hello', src='en', dest='chinese (simplified)')
+    assert result.dest == 'zh-cn'
+
+
 def test_language_rfc1766(translator):
     result = translator.translate(u'luna', src='it_ch@euro', dest='en')
     assert result.text == u'moon'
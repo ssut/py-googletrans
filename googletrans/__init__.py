a8c265583765e8b1f8e1f393487c2d49e47a49df
SuHun Han <ssut@ssut.me>
2017-08-11 22:04:15 +0900

    Bump version to 2.1.4

--- googletrans/__init__.py
+++ googletrans/__init__.py
@@ -1,6 +1,6 @@
 """Free Google Translate API for Python. Translates totally free of charge."""
 __all__ = 'Translator',
-__version__ = '2.1.3'
+__version__ = '2.1.4'
 
 
 from googletrans.client import Translator

296449efb09871063431c3d11e94a6dc38458bdc
Simone Esposito <chaufnet@gmail.com>
2018-09-23 09:47:02 +0200

    Bump version to 3.2.1

--- aiogoogletrans/__init__.py
+++ aiogoogletrans/__init__.py
@@ -1,6 +1,6 @@
 """Free Google Translate API for Python. Translates totally free of charge."""
 __all__ = 'Translator',
-__version__ = '3.2.0'
+__version__ = '3.2.1'
 
 
 from aiogoogletrans.client import Translator

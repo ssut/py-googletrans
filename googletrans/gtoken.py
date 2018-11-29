506ec8e5a161045b1effaffec565b1a704e392c8
guydipietro <guy_dipietro@bose.com>
2018-11-29 10:48:50 -0500

    Updating regex used to extract gtoken

--- googletrans/gtoken.py
+++ googletrans/gtoken.py
@@ -38,9 +38,8 @@ class TokenAcquirer(object):
         950629.577246
     """
 
-    RE_TKK = re.compile(r'TKK=eval\(\'\(\(function\(\)\{(.+?)\}\)\(\)\)\'\);',
-                        re.DOTALL)
-    RE_RAWTKK = re.compile(r'TKK=\'([^\']*)\';', re.DOTALL)
+    RE_TKK = re.compile(r'tkk:\'(.+?)\'', re.DOTALL)
+    RE_RAWTKK = re.compile(r'tkk:\'(.+?)\'', re.DOTALL)
 
     def __init__(self, tkk='0', session=None, host='translate.google.com'):
         self.session = session or requests.Session()

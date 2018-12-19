194d65a2e281db99b033f3e421583ae87d48fab8
SuHun Han <ssut@ssut.me>
2018-12-20 01:04:17 +0900

    chore: bump to 2.4.0

--- setup.py
+++ setup.py
@@ -55,7 +55,8 @@ def install():
                      'Programming Language :: Python :: 2.7',
                      'Programming Language :: Python :: 3.4',
                      'Programming Language :: Python :: 3.5',
-                     'Programming Language :: Python :: 3.6'],
+                     'Programming Language :: Python :: 3.6',
+                     'Programming Language :: Python :: 3.7'],
         packages=find_packages(exclude=['docs', 'tests']),
         keywords='google translate translator',
         install_requires=[

4aebfb18faa45a7d7817fbd4b8fe8ff502bf9e81
Nieole <saber00sword@gmail.com>
2019-02-05 16:30:09 +0800

    Update utils (#102)

--- googletrans/__init__.py
+++ googletrans/__init__.py
@@ -1,6 +1,6 @@
 """Free Google Translate API for Python. Translates totally free of charge."""
 __all__ = 'Translator',
-__version__ = '2.4.0'
+__version__ = '2.4.1'
 
 
 from googletrans.client import Translator

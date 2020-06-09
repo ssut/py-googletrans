f968008cdcea4e9c7bd4bbcb6f1c5936993e001a
SuHun Han <ssut@ssut.me>
2020-06-09 10:31:45 +0900

    chore(python): drop support for 3.5 in favor of httpx

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ def install():
                      'Operating System :: MacOS :: MacOS X',
                      'Topic :: Education',
                      'Programming Language :: Python',
-                     'Programming Language :: Python :: 3.5',
                      'Programming Language :: Python :: 3.6',
                      'Programming Language :: Python :: 3.7',
                      'Programming Language :: Python :: 3.8'],

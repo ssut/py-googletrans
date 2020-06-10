cfee004933353d8d47c2de416059d8bef4c24756
terryzhuo110 <terryzhuo25@gmail.com>
2020-06-10 18:56:36 +1000

    storing available urls as constant

--- googletrans/client.py
+++ googletrans/client.py
@@ -11,7 +11,7 @@ from googletrans import urls, utils
 from googletrans.adapters import TimeoutAdapter
 from googletrans.compat import PY3
 from googletrans.gtoken import TokenAcquirer
-from googletrans.constants import DEFAULT_USER_AGENT, DEFAULT_SERVICE_URLS, LANGCODES, LANGUAGES, SPECIAL_CASES
+from googletrans.constants import DEFAULT_USER_AGENT, LANGCODES, LANGUAGES, SPECIAL_CASES
 from googletrans.models import Translated, Detected
 
 
@@ -43,10 +43,8 @@ class Translator(object):
     """
 
     def __init__(self, service_urls=None, user_agent=DEFAULT_USER_AGENT,
-                 proxies=None, timeout=None, available_service_list=None):
+                 proxies=None, timeout=None):
 
-        self.available_service_list = DEFAULT_SERVICE_URLS
-            
         self.session = requests.Session()
         if proxies is not None:
             self.session.proxies = proxies

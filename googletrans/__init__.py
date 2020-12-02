1ed95815f3ae2945ecfc5f7d29d0bde6ae1fd34c
Suhun Han <ssut@ssut.me>
2020-12-02 22:20:23 +0900

    chore: 3.1.0-alpha

--- googletrans/__init__.py
+++ googletrans/__init__.py
@@ -1,6 +1,6 @@
 """Free Google Translate API for Python. Translates totally free of charge."""
 __all__ = 'Translator',
-__version__ = '3.0.0'
+__version__ = '3.1.0-alpha'
 
 
 from googletrans.client import Translator

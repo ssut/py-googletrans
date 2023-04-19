b7be8a1e598cafaf566a44ee75023aa5203c2fa6
bllen <bllendev@gmail.com>
2023-04-19 18:25:21 -0500

    cleaner test code

--- tests/test_gtoken.py
+++ tests/test_gtoken.py
@@ -13,17 +13,13 @@ def acquirer():
 
 def test_acquire_token(acquirer):
     text = 'test'
-
     result = acquirer.do(text)
-
     assert result
 
 
 def test_acquire_token_ascii_less_than_2048(acquirer):
     text = u'Ѐ'
-
     result = acquirer.do(text)
-
     assert result
 
 
@@ -34,33 +30,25 @@ def test_acquire_token_ascii_matches_special_condition(acquirer):
         except NameError:
             return chr(i)
     text = unichar(55296) + unichar(56320)
-
     result = acquirer.do(text)
-
     assert result
 
 
 def test_acquire_token_ascii_else(acquirer):
     text = u'가'
-
     result = acquirer.do(text)
-
     assert result
 
 
 def test_reuse_valid_token(acquirer):
     text = 'test'
-
     first = acquirer.do(text)
     second = acquirer.do(text)
-
     assert first == second
 
 
 def test_map_lazy_return(acquirer):
     value = True
-
     func = acquirer._lazy(value)
-
     assert callable(func)
     assert func() == value

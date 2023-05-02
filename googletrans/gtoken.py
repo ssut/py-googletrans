2c0f05392c512f62b4e94b785acb951e11f05e25
bllen <bllendev@gmail.com>
2023-05-01 19:02:46 -0600

    lower fix

--- googletrans/gtoken.py
+++ googletrans/gtoken.py
@@ -64,7 +64,7 @@ class TokenAcquirer:
         try:
             code = self.RE_RAWTKK.search(r.text).group(1)
         except Exception as e:
-            if r.text and "<!doctype html>" not in r.text:
+            if r.text and "<!doctype html>" not in r.text.lower():
                 print()
                 print(f"{AttributeError('py-googletrans warning: TKK is not found')} - {e} - original input text: {r.text[:200]}")
                 print()
